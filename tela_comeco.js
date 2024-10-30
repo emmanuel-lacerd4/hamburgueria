b3603b29e78f106b10fa4c1a04a6174f02ad54ab
Emmanuel L. Nogueira <emmanuellacerd4@gmail.com>
2024-10-30 19:39:52 -0300

    hamburgueria

---
+++ tela_comeco.js
@@ -0,0 +1,45 @@
+// Animação da tela inicial
+document.querySelector('#tela_comeco').addEventListener('click', () => {
+    document.querySelector('#tela_comeco').classList.add("subir")
+})
+
+// Animação do menu lateral
+document.querySelector('#promocoes').addEventListener('click', () => {
+    document.querySelector('#promocoes').classList.add("crescer")
+    document.querySelector('#hamburguers').classList.remove("crescer")
+    document.querySelector('#acompanhamentos').classList.remove("crescer")
+    document.querySelector('#bebidas').classList.remove("crescer")
+    document.querySelector('#sobremesas').classList.remove("crescer")
+})
+
+document.querySelector('#hamburguers').addEventListener('click', () => {
+    document.querySelector('#hamburguers').classList.add("crescer")
+    document.querySelector('#promocoes').classList.remove("crescer")
+    document.querySelector('#acompanhamentos').classList.remove("crescer")
+    document.querySelector('#bebidas').classList.remove("crescer")
+    document.querySelector('#sobremesas').classList.remove("crescer")
+})
+
+document.querySelector('#acompanhamentos').addEventListener('click', () => {
+    document.querySelector('#acompanhamentos').classList.add("crescer")
+    document.querySelector('#promocoes').classList.remove("crescer")
+    document.querySelector('#hamburguers').classList.remove("crescer")
+    document.querySelector('#bebidas').classList.remove("crescer")
+    document.querySelector('#sobremesas').classList.remove("crescer")
+})
+
+document.querySelector('#bebidas').addEventListener('click', () => {
+    document.querySelector('#bebidas').classList.add("crescer")
+    document.querySelector('#promocoes').classList.remove("crescer")
+    document.querySelector('#hamburguers').classList.remove("crescer")
+    document.querySelector('#acompanhamentos').classList.remove("crescer")
+    document.querySelector('#sobremesas').classList.remove("crescer")
+})
+
+document.querySelector('#sobremesas').addEventListener('click', () => {
+    document.querySelector('#sobremesas').classList.add("crescer")
+    document.querySelector('#promocoes').classList.remove("crescer")
+    document.querySelector('#hamburguers').classList.remove("crescer")
+    document.querySelector('#acompanhamentos').classList.remove("crescer")
+    document.querySelector('#bebidas').classList.remove("crescer")
+})
